0aaaf6b4ba83fda488675b9b391bd23ec5f43f52
rique lima <riquelmedeelim.a@gmail.com>
2026-09-06 20:50:33 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.decentstorage.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0-mvp"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     buildTypes {
